bd7eccacbfbf0035d323fbf46f045d41aaf7208d
Ben Diamand <ben@hito-muni.com>
2022-08-23 14:50:42 +0300

    deleted inneccesery files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ phonetic_dict = {nato.letter: nato.code for (index, nato) in nato_data.iterrows(
 word = input("Enter a word: ").upper()
 phonetic_word = [phonetic_dict[letter] for letter in word]
 print(phonetic_word)
+
 for nato_word in phonetic_word:
     sound = os.path.dirname(__file__) + f"/sounds/{nato_word}.mp3"
     playsound(sound)
