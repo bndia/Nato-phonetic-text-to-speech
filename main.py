ecf7dde3d2032a0d3ec08d059b3e5c8dab4416ab
Ben Diamand <ben@hito-muni.com>
2022-08-23 13:50:02 +0300

    final

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas
+import os
+import gtts
+from playsound import playsound
+
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {nato.letter: nato.code for (index, nato) in nato_data.iterrows()}
+
+word = input("Enter a word: ").upper()
+phonetic_word = [phonetic_dict[letter] for letter in word]
+print(phonetic_word)
+for nato_word in phonetic_word:
+    sound = os.path.dirname(__file__) + f"/sounds/{nato_word}.mp3"
+    playsound(sound)
+    # tts = gtts.gTTS(f"{nato_word}", lang="en")
+    # tts.save(f"{nato_word}.mp3")
+    # playsound(f"{nato_word}.mp3")
+    # os.remove(f"{nato_word}.mp3")
